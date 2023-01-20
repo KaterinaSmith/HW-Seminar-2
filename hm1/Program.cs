944e440d46da64b02d6c91e0a99064cd241839a5
Katerina <katerina3113@bk.ru>
2023-01-20 12:42:35 +0300

    Домашнее задание 1

---
+++ hm1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+
+{
+int a = Convert.ToInt32(number);
+int b = (a / 10) % 10;
+Console.WriteLine(b);
+}
