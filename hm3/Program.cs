ea6c154f380cbe283b90fe49f20e1b8ae4b5c4fa
Katerina <katerina3113@bk.ru>
2023-01-22 01:17:08 +0300

    Домашнее задание 3

---
+++ hm3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine ("Введите порядковый номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6)
+{
+    Console.WriteLine("Это выходной день");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else if (number <=5)
+{
+    Console.WriteLine("Это рабочий день");
+}
+else 
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
