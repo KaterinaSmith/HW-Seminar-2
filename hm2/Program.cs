3ef478db6f17fee629e89323212e9c36629ad57b
Katerina <katerina3113@bk.ru>
2023-01-22 00:53:52 +0300

    Сохранили дз2

---
+++ hm2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+int ReadInt (string message)
+{
+Console.WriteLine("Введите число:");
+return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b < 3)
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+    }
+    else 
+    {
+        int c = 1;
+        for (int i = b, i > 3; i--);
+    {
+        c = c * 10;
+    }
+    result = (a / c ) % 10;
+    }
+    return result;
+}
